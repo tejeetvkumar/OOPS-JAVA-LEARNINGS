fca9e6d692faf0f48fd2b818ee61528d4d361967
tejeetvkumar <tejeet.work@gmail.com>
2026-09-11 12:13:48 +0530

    Add Shape polymorphism example with Circle and Rectangle

--- ShapeTest.java
+++ ShapeTest.java
@@ -1,47 +1,12 @@
-class Shape {
-    void area() {
-        System.out.println("Area not defined for generic shape");
-    }
-}
-
-class Circle extends Shape {
-    double radius;
-
-    Circle(double radius) {
-        this.radius = radius;
-    }
-
-    @Override
-    void area() {
-        System.out.println("Circle area: " + (Math.PI * radius * radius));
-    }
-}
-
-class Rectangle extends Shape {
-    double length, width;
-
-    Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
-    }
-
-    @Override
-    void area() {
-        System.out.println("Rectangle area: " + (length * width));
-    }
-}
-
 public class ShapeTest {
     public static void main(String[] args) {
-        Shape s;
+        Shape[] shapes = new Shape[2];
 
-        s = new Circle(5.0);
-        s.area();
+        shapes[0] = new Circle(5.0);
+        shapes[1] = new Rectangle(4.0, 6.0);
 
-        s = new Rectangle(4.0, 6.0);
-        s.area();
-
-        s = new Shape();
-        s.area();
+        for (Shape s : shapes) {
+            s.displayArea();
+        }
     }
 }
